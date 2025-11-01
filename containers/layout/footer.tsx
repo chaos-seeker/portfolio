def590c2c4cc8fb941845b33a0eabf927fc5779d
hamid shahsavani <051.sys113@gmail.com>
2025-11-01 18:02:00 +0330

    chore: new changes

--- containers/layout/footer.tsx
+++ containers/layout/footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer>
       <div className="container">
         <footer className="text-muted-foreground pt-16 pb-8 text-center text-sm">
-          <p>©2025 Hamid Shahsavani. All rights reserved.</p>
+          <p>©2025 hamid shahsavani. All rights reserved.</p>
           <p className="mt-1">developed with ❤️</p>
         </footer>
       </div>
