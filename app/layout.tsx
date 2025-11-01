a59da269425fa18eb689aad1c293b8246656b8c4
hamid shahsavani <051.sys113@gmail.com>
2025-11-01 12:47:28 +0330

    feat: add shadcnui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "portfolio",
 };
+const fontInter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-inter',
+});
 
 export default function RootLayout({
   children,
@@ -12,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={fontInter.className}>
         {children}
       </body>
     </html>
