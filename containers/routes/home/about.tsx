e24dca9397c6dc7fa753d3d658d38a6fd6527e89
hamid shahsavani <051.sys113@gmail.com>
2025-11-01 17:06:04 +0330

    chore: new changes

--- containers/routes/home/about.tsx
+++ containers/routes/home/about.tsx
@@ -6,13 +6,11 @@ export const About = () => {
           <h2 className="text-primary mb-6 font-mono text-2xl font-bold">
             About
           </h2>
-          <div className="space-y-4">
+          <div className="space-y-2">
             <p className="text-muted-foreground text-mdp">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Accusamus ex voluptatum, nulla explicabo velit incidunt delectus
-              debitis molestiae, nihil voluptatibus fugit cumque harum
-              assumenda, fugiat in tempore! Reiciendis, cum quasi.
+            i design with precision and obsessive attention to detail, then translate those designs into production-ready interfaces.
             </p>
+            <p className="text-muted-foreground text-mdp">My code follows best practices, emphasizes performance, and remains clean, readable, and easy to maintain.</p>
           </div>
         </div>
       </div>
--- containers/routes/home/skills.tsx
+++ containers/routes/home/skills.tsx
@@ -10,19 +10,21 @@ export const Skills = () => {
           </h2>
           <div className="flex grid grid-cols-1 flex-col gap-4 lg:grid-cols-2">
             <ProgressItem title="HTML" value={90} />
-            <ProgressItem title="CSS" value={80} />
-            <ProgressItem title="JavaScript" value={70} />
-            <ProgressItem title="React" value={60} />
-            <ProgressItem title="Next.js" value={50} />
-            <ProgressItem title="Node.js" value={40} />
-            <ProgressItem title="Express" value={30} />
-            <ProgressItem title="MongoDB" value={20} />
-            <ProgressItem title="MySQL" value={10} />
-            <ProgressItem title="PostgreSQL" value={5} />
-            <ProgressItem title="Redis" value={3} />
-            <ProgressItem title="Docker" value={2} />
-            <ProgressItem title="Kubernetes" value={1} />
-            <ProgressItem title="Git" value={0} />
+            <ProgressItem title="CSS" value={90} />
+            <ProgressItem title="JavaScript" value={90} />
+            <ProgressItem title="TypeScript" value={80} />
+            <ProgressItem title="React" value={90} />
+            <ProgressItem title="Next.js" value={850} />
+            <ProgressItem title="Tailwind" value={98} />
+            <ProgressItem title="Git" value={80} />
+            <ProgressItem title="Shadcnui" value={97} />
+            <ProgressItem title="daisyui" value={95} />
+            <ProgressItem title="heroui" value={95} />
+            <ProgressItem title="npm" value={90} />
+            <ProgressItem title="figma" value={70} />
+            <ProgressItem title="wordpress" value={70} />
+            <ProgressItem title="elmentor" value={60} />
+            <ProgressItem title="linux" value={75} />
           </div>
         </div>
       </div>
