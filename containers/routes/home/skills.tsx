9814855ef43b5a33a1ceecce53165dd75fca3687
hamid shahsavani <051.sys113@gmail.com>
2025-11-15 10:43:55 +0330

    chore: new changes

--- containers/routes/home/skills.tsx
+++ containers/routes/home/skills.tsx
@@ -1,11 +1,29 @@
 'use client';
 
-import { Progress } from '@/ui/progress';
 import Link from 'next/link';
 import { useTranslation } from '@/hooks/translation';
 
 export const Skills = () => {
   const { t } = useTranslation();
+
+  const techKeys = [
+    'tech.html',
+    'tech.css',
+    'tech.javascript',
+    'tech.typescript',
+    'tech.react',
+    'tech.nextjs',
+    'tech.tailwind',
+    'tech.git',
+    'tech.shadcnui',
+    'tech.daisyui',
+    'tech.heroui',
+    'tech.npm',
+    'tech.figma',
+    'tech.wordpress',
+    'tech.elementor',
+    'tech.linux',
+  ];
 
   return (
     <section>
@@ -23,43 +41,18 @@ export const Skills = () => {
               {t('skills.title')}
             </h2>
           </div>
-          <div className="grid grid-cols-1 flex-col gap-4 lg:grid-cols-2">
-            <ProgressItem techKey="tech.html" value={90} />
-            <ProgressItem techKey="tech.css" value={90} />
-            <ProgressItem techKey="tech.javascript" value={90} />
-            <ProgressItem techKey="tech.typescript" value={80} />
-            <ProgressItem techKey="tech.react" value={90} />
-            <ProgressItem techKey="tech.nextjs" value={85} />
-            <ProgressItem techKey="tech.tailwind" value={98} />
-            <ProgressItem techKey="tech.git" value={80} />
-            <ProgressItem techKey="tech.shadcnui" value={97} />
-            <ProgressItem techKey="tech.daisyui" value={95} />
-            <ProgressItem techKey="tech.heroui" value={95} />
-            <ProgressItem techKey="tech.npm" value={90} />
-            <ProgressItem techKey="tech.figma" value={70} />
-            <ProgressItem techKey="tech.wordpress" value={70} />
-            <ProgressItem techKey="tech.elementor" value={60} />
-            <ProgressItem techKey="tech.linux" value={75} />
-          </div>
+          <ul className="flex flex-wrap gap-4">
+            {techKeys.map((techKey) => (
+              <li key={techKey} className="flex items-center gap-1">
+                <span className="text-primary">•</span>
+                <span className="text-smp font-medium">
+                  {t(techKey as any)}
+                </span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
-  );
-};
-
-interface ProgressItemProps {
-  techKey: string;
-  value: number;
-}
-
-const ProgressItem = (props: ProgressItemProps) => {
-  const { t } = useTranslation();
-  return (
-    <div className="flex flex-col gap-1">
-      <div className="flex w-full items-center justify-between gap-2">
-        <span className="text-smp font-medium">{t(props.techKey as any)}</span>
-      </div>
-      <Progress value={props.value} />
-    </div>
   );
 };
